3492d92b8801d628e2534e637bdec10f82a018d4
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2022-05-08 20:54:26 -0300

    Changed message running

--- src/main/java/br/com/lucolimac/ministerialscaleapi/MinisterialScaleApiApplication.java
+++ src/main/java/br/com/lucolimac/ministerialscaleapi/MinisterialScaleApiApplication.java
@@ -5,17 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableWebMvc
-@EnableSwagger2
 @SpringBootApplication
 public class MinisterialScaleApiApplication {
     private static final Logger logger = LoggerFactory.getLogger(MinisterialScaleApiApplication.class);
 
     public static void main(String[] args) {
-        logger.info("Iniciando aplicação...");
+        logger.info("Starting application...");
         SpringApplication.run(MinisterialScaleApiApplication.class, args);
-        logger.info("A aplicação Scale está rodando!");
+        logger.info("The application is running!");
     }
 }
